767fa4a135cffca7f6a2ab340f78003507afdc4a
Pierre Feyssaguet <pfeyssaguet@gmail.com>
2015-11-09 08:51:14 +0100

    Code style

--- Entity/Repository/CronTaskLogRepository.php
+++ Entity/Repository/CronTaskLogRepository.php
@@ -13,36 +13,36 @@ use DspSofts\CronManagerBundle\Entity\CronTaskLog;
 
 class CronTaskLogRepository extends EntityRepository
 {
-	/**
-	 * @return CronTaskLog[]
-	 */
-	public function findByPidNotNull()
-	{
-		$queryBuilder = $this->createQueryBuilder('cron_task_log');
-		$queryBuilder->where('cron_task_log.pid IS NOT NULL');
-		$queryBuilder->orderBy('cron_task_log.dateStart', 'desc');
+    /**
+     * @return CronTaskLog[]
+     */
+    public function findByPidNotNull()
+    {
+        $queryBuilder = $this->createQueryBuilder('cron_task_log');
+        $queryBuilder->where('cron_task_log.pid IS NOT NULL');
+        $queryBuilder->orderBy('cron_task_log.dateStart', 'desc');
 
-		$query = $queryBuilder->getQuery();
+        $query = $queryBuilder->getQuery();
 
-		return $query->getResult();
-	}
+        return $query->getResult();
+    }
 
-	/**
-	 * @param \DateTime $dateTime
-	 * @return CronTaskLog[]
-	 */
-	public function findFinishedByDate(\DateTime $dateTime)
-	{
-		$queryBuilder = $this->createQueryBuilder('cron_task_log');
-		$queryBuilder->select('cron_task_log');
-		$queryBuilder->where('cron_task_log.pid IS NULL');
-		$queryBuilder->andWhere('cron_task_log.dateStart >= :dateStart');
-		//$queryBuilder->groupBy('cron_task_log.cronTask, cron_task_log.status');
-		$queryBuilder->orderBy('cron_task_log.dateStart', 'desc');
+    /**
+     * @param \DateTime $dateTime
+     * @return CronTaskLog[]
+     */
+    public function findFinishedByDate(\DateTime $dateTime)
+    {
+        $queryBuilder = $this->createQueryBuilder('cron_task_log');
+        $queryBuilder->select('cron_task_log');
+        $queryBuilder->where('cron_task_log.pid IS NULL');
+        $queryBuilder->andWhere('cron_task_log.dateStart >= :dateStart');
+        //$queryBuilder->groupBy('cron_task_log.cronTask, cron_task_log.status');
+        $queryBuilder->orderBy('cron_task_log.dateStart', 'desc');
 
-		$queryBuilder->setParameters(array('dateStart' => $dateTime));
-		$query = $queryBuilder->getQuery();
+        $queryBuilder->setParameters(array('dateStart' => $dateTime));
+        $query = $queryBuilder->getQuery();
 
-		return $query->getResult();
-	}
+        return $query->getResult();
+    }
 }
