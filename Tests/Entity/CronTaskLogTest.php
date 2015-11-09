ee8782f405ff5c2430bfbfd39f68c9be4149bd12
Pierre Feyssaguet <pfeyssaguet@gmail.com>
2015-11-09 15:31:37 +0100

    Splitted tests in several methods

--- Tests/Entity/CronTaskLogTest.php
+++ Tests/Entity/CronTaskLogTest.php
@@ -11,27 +11,48 @@ use DspSofts\CronManagerBundle\Entity\CronTaskLog;
 
 class CronTaskLogTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFields()
-    {
-        $cronTaskLog = new CronTaskLog();
+	public function testId()
+	{
+		$cronTaskLog = new CronTaskLog();
+		$this->assertNull($cronTaskLog->getId());
+	}
 
-        $this->assertNull($cronTaskLog->getId());
+	public function testStatus()
+	{
+		$cronTaskLog = new CronTaskLog();
+		$cronTaskLog->setStatus(CronTaskLog::STATUS_SUCCESS);
+		$this->assertEquals(CronTaskLog::STATUS_SUCCESS, $cronTaskLog->getStatus());
+	}
 
-        $cronTaskLog->setStatus(CronTaskLog::STATUS_SUCCESS);
-        $this->assertEquals(CronTaskLog::STATUS_SUCCESS, $cronTaskLog->getStatus());
+	public function testDateStart()
+	{
+		$testDate = new \DateTime();
+		$cronTaskLog = new CronTaskLog();
+		$cronTaskLog->setDateStart($testDate);
+		$this->assertEquals($testDate, $cronTaskLog->getDateStart());
+	}
 
-        $testDate = new \DateTime();
-        $cronTaskLog->setDateStart($testDate);
-        $this->assertEquals($testDate, $cronTaskLog->getDateStart());
+	public function testDateEnd()
+	{
+		$testDate = new \DateTime();
+		$cronTaskLog = new CronTaskLog();
+		$this->assertNull($cronTaskLog->getDateEnd());
+		$cronTaskLog->setDateEnd($testDate);
+		$this->assertEquals($testDate, $cronTaskLog->getDateEnd());
+	}
 
-        $this->assertNull($cronTaskLog->getDateEnd());
-        $cronTaskLog->setDateEnd($testDate);
-        $this->assertEquals($testDate, $cronTaskLog->getDateEnd());
+	public function testFilePath()
+	{
+		$cronTaskLog = new CronTaskLog();
+		$cronTaskLog->setFilePath('test');
+		$this->assertEquals('test', $cronTaskLog->getFilePath());
+	}
 
-        $cronTaskLog->setFilePath('test');
-        $this->assertEquals('test', $cronTaskLog->getFilePath());
-
-        $cronTaskLog->setPid('123');
-        $this->assertEquals('123', $cronTaskLog->getPid());
-    }
+	public function testPid()
+	{
+		$cronTaskLog = new CronTaskLog();
+		$cronTaskLog->setPid(123);
+		$this->assertEquals(123, $cronTaskLog->getPid());
+	}
 }
+
